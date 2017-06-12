7e2f6d4b50b2a5f6c184114425a1c7ab12c80d3d
cice <cice@192.168.3.67>
2017-06-12 21:55:28 +0200

    (INITIAL COMMIT)

---
+++ src/com/cice/principal/HolaMundo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cice.principal;
+
+/**
+ *
+ * @author cice
+ */
+public class HolaMundo {
+    
+    private static final String SALUDO = " Hola Mundo";
+    
+    public static void main(String[]arg){
+    
+        System.out.println(SALUDO);
+    
+    }
+    
+    
+    
+}
